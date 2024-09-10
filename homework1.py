dcba1106c10e08567fe193f88e2a1b0d76fc2354
alve071211 <alve071211@gmail.com>
2024-09-10 17:09:41 +1200

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,5 @@
+number_of_tasks=12
+hour=1.5
+course='Python'
+time=hour/number_of_tasks
+print('Курс:',course,', всего задач',number_of_tasks,', затрачено часов',hour,', среднее время выполнения',time,'часа.')
